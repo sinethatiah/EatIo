30c9d3c958f71fad7e4807efb3e28bf4b11afb45
thatiah <thatiahnia@gmail.com>
2026-05-21 11:41:26 +0300

    change source of logo

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
 
           <div className="mb-8 flex items-center gap-2">
            <img
-      src="/public/myLogo.png"
+      src="/myLogo.png"
       alt="eat.io"
       className="w-12 h-12 rounded-xl"
       style={{ backgroundColor: "#5c3318" }}
